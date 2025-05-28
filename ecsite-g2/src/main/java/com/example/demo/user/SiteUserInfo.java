300fc5c9a885432a7bb92ff56299248d9b793a40
amemiya <amemiya@ia.com>
2025-05-28 13:13:27 +0900

    SiteUserInfo.javaの作成

---
+++ ecsite-g2/src/main/java/com/example/demo/user/SiteUserInfo.java
@@ -0,0 +1,127 @@
+package com.example.demo.user;
+
+public class SiteUserInfo {
+
+	private Integer ID;
+	private String gender;
+	private Integer postNumber;
+	private String address1;
+	private String address2;
+	private String address3;
+	private String address4;
+	private Integer creditNumber;
+	private String birthday;
+	private String firstName1;
+	private String lastName1;
+	private String firstName2;
+	private String lastName2;
+
+	public SiteUserInfo() {
+		super();
+	}
+
+	public Integer getID() {
+		return ID;
+	}
+
+	public void setID(Integer iD) {
+		ID = iD;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Integer getPostNumber() {
+		return postNumber;
+	}
+
+	public void setPostNumber(Integer postNumber) {
+		this.postNumber = postNumber;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getAddress3() {
+		return address3;
+	}
+
+	public void setAddress3(String address3) {
+		this.address3 = address3;
+	}
+
+	public String getAddress4() {
+		return address4;
+	}
+
+	public void setAddress4(String address4) {
+		this.address4 = address4;
+	}
+
+	public Integer getCreditNumber() {
+		return creditNumber;
+	}
+
+	public void setCreditNumber(Integer creditNumber) {
+		this.creditNumber = creditNumber;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getFirstName1() {
+		return firstName1;
+	}
+
+	public void setFirstName1(String firstName1) {
+		this.firstName1 = firstName1;
+	}
+
+	public String getLastName1() {
+		return lastName1;
+	}
+
+	public void setLastName1(String lastName1) {
+		this.lastName1 = lastName1;
+	}
+
+	public String getFirstName2() {
+		return firstName2;
+	}
+
+	public void setFirstName2(String firstName2) {
+		this.firstName2 = firstName2;
+	}
+
+	public String getLastName2() {
+		return lastName2;
+	}
+
+	public void setLastName2(String lastName2) {
+		this.lastName2 = lastName2;
+	}
+
+}
