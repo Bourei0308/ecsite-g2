3e2e38f8bc13ea50667c74eadfae823e040c1dfe
yang <2430371802@qq.com>
2025-05-28 14:07:59 +0900

    mypage.html

--- ecsite-g2/src/main/java/com/example/demo/userEdit/UserEditController.java
+++ ecsite-g2/src/main/java/com/example/demo/userEdit/UserEditController.java
@@ -1,5 +1,31 @@
 package com.example.demo.userEdit;
 
-public class UserEditController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.dao.Dao;
+import com.example.demo.user.SiteUser;
+import com.example.demo.user.SiteUserInfo;
+
+
+@Controller
+public class UserEditController {
+	private final UserInfoDao infodb;
+	private final Dao db;
+	
+	public UserEditController (UserInfoDao infodb,Dao db) {
+		this.db = db;
+		this.infodb = infodb;
+	}
+	
+	@RequestMapping("/mypage")
+	public String data(
+			Model model,SiteUser su,SiteUserInfo info) {
+		int ID = su.getID();
+		if(info ==null) {
+			info = infodb.getById(ID);
+		}
+		return "mypage";
+	}
 }
