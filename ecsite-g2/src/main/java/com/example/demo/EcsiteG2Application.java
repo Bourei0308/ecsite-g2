fb6e3e4f67d6b044f5dbe0f931f7bf05eb3b193d
yang <2430371802@qq.com>
2025-05-28 09:05:00 +0900

    Initial commit

---
+++ ecsite-g2/src/main/java/com/example/demo/EcsiteG2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsiteG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsiteG2Application.class, args);
+	}
+
+}
