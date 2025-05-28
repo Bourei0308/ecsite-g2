f5203fb9ac92d5bfd34ce38f11c44e5defe444a8
amemiya <amemiya@ia.com>
2025-05-28 12:59:06 +0900

    SiteUser.javaのタイポ修正

--- ecsite-g2/src/main/java/com/example/demo/user/SiteUser.java
+++ ecsite-g2/src/main/java/com/example/demo/user/SiteUser.java
@@ -4,91 +4,89 @@ import java.sql.Timestamp;
 
 public class SiteUser {
 
-    private Integer id;
-    private String password;
-    private String nickName;
-    private Boolean adminFlag;
-    private Boolean deleteFlag;
-    private String email;
-    private Timestamp created_at;
-    private Timestamp login_at;
-    private String phone_number;
+	private Integer ID;
+	private String password;
+	private String nickName;
+	private Boolean adminFlag;
+	private Boolean deleteFlag;
+	private String email;
+	private Timestamp created_at;
+	private Timestamp login_at;
+	private String phone_number;
 
-    
-    public SiteUser() {
+	public SiteUser() {
 		super();
 	}
 
-	public Integer getId() {
-        return id;
-    }
+	public Integer getID() {
+		return ID;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer ID) {
+		this.ID = ID;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public String getNickName() {
-        return nickName;
-    }
+	public String getNickName() {
+		return nickName;
+	}
 
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
 
-    public Boolean getAdminFlag() {
-        return adminFlag;
-    }
+	public Boolean getAdminFlag() {
+		return adminFlag;
+	}
 
-    public void setAdminFlag(Boolean adminFlag) {
-        this.adminFlag = adminFlag;
-    }
+	public void setAdminFlag(Boolean adminFlag) {
+		this.adminFlag = adminFlag;
+	}
 
-    public Boolean getDeleteFlag() {
-        return deleteFlag;
-    }
+	public Boolean getDeleteFlag() {
+		return deleteFlag;
+	}
 
-    public void setDeleteFlag(Boolean deleteFlag) {
-        this.deleteFlag = deleteFlag;
-    }
+	public void setDeleteFlag(Boolean deleteFlag) {
+		this.deleteFlag = deleteFlag;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public Timestamp getCreated_at() {
-        return created_at;
-    }
+	public Timestamp getCreated_at() {
+		return created_at;
+	}
 
-    public void setCreated_at(Timestamp created_at) {
-        this.created_at = created_at;
-    }
+	public void setCreated_at(Timestamp created_at) {
+		this.created_at = created_at;
+	}
 
-    public Timestamp getLogin_at() {
-        return login_at;
-    }
+	public Timestamp getLogin_at() {
+		return login_at;
+	}
 
-    public void setLogin_at(Timestamp login_at) {
-        this.login_at = login_at;
-    }
+	public void setLogin_at(Timestamp login_at) {
+		this.login_at = login_at;
+	}
 
-    public String getPhone_number() {
-        return phone_number;
-    }
+	public String getPhone_number() {
+		return phone_number;
+	}
 
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
 }
-
