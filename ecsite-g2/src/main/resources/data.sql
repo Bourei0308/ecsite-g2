5df23c2dcfd981bf432319903a3536547310dbae
wangmengyuan <wangmengyuan@ia.com>
2025-05-28 11:40:14 +0900

    SiteUserの作成

--- ecsite-g2/src/main/resources/data.sql
+++ ecsite-g2/src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO SiteUser (name) VALUES ('Taro');
+INSERT INTO SiteUser (password,nickName,adminFlag,deleteFlag,email,phone_number) VALUES ('1234','Taro',0,0,'a@a.com','123456');
--- ecsite-g2/src/main/resources/schema.sql
+++ ecsite-g2/src/main/resources/schema.sql
@@ -1,5 +1,11 @@
 CREATE TABLE IF NOT EXISTS SiteUser(
-ID INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(100) NOT NULL,
-PRIMARY KEY(ID)
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    password VARCHAR(100) NOT NULL,
+    nickName VARCHAR(100) NOT NULL,
+    adminFlag TINYINT(1) NOT NULL COMMENT '0 or 1',
+    deleteFlag TINYINT(1) NOT NULL COMMENT '0 or 1',
+    email VARCHAR(100),
+    created_at TIMESTAMP,
+    login_at TIMESTAMP,
+    phone_number VARCHAR(20)
 );
