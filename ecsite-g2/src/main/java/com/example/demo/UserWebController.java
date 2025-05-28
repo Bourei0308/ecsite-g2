01bfb4044ccad47bc312e458fbfda3890069444e
amemiya <amemiya@ia.com>
2025-05-28 14:06:56 +0900

    顧客のログイン・サインインの画面遷移まで作成

--- ecsite-g2/src/main/java/com/example/demo/UserWebController.java
+++ ecsite-g2/src/main/java/com/example/demo/UserWebController.java
@@ -1,5 +1,38 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.dao.Dao;
+
+@Controller
 public class UserWebController {
+
+	private final Dao dao;
+
+	public UserWebController(Dao dao) {
+		this.dao = dao;
+	}
+
+	@RequestMapping("/index")
+	public String index(Model model) {
+		return "index";
+	}
+
+	@RequestMapping("/signIn")
+	public String signIn(Model model) {
+		return "login/signIn";
+	}
+
+	@RequestMapping("/confirm")
+	public String confirm(Model model) {
+		return "login/confirm";
+	}
+
+	@RequestMapping("/complete")
+	public String complete(Model model) {
+		return "login/complete";
+	}
 
 }
